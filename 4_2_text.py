213674108266cc7f4bae6a9b17f3d613439a07da
daeyonw <62833187+daeyonw@users.noreply.github.com>
2022-12-06 21:16:23 +0900

    Add files via upload

---
+++ 4_2_text.py
@@ -0,0 +1,11 @@
+print('Hello world')
+print("Hello world")
+print('''
+Hello
+world
+''')
+
+print("'1'+'1'", '1'+'1')
+print('Hello world'*10)
+print("len('Hello world'*10)", len('Hello world'*10))
+print("'hello world'.replace('world', 'universe')", 'hello world'.replace('world', 'universe'))
